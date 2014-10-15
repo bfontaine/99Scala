72cac471e889685c1e0e27d9e418dd39da3dcd51
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-15 23:18:22 +0200

    P07: unchecked warning fixed

--- src/main/scala/P07.scala
+++ src/main/scala/P07.scala
@@ -4,7 +4,7 @@ object P07 {
 
   private def flattenAcc[T](source : List[T], target : List[T]) : List[T] = source match {
       case Nil => target
-      case (h:List[T]) :: tail =>
+      case (h:List[T @unchecked]) :: tail =>
         flattenAcc(tail, P05.reverse(flatten(h)) ::: target)
       case h :: tail => flattenAcc(tail, h :: target)
     }
