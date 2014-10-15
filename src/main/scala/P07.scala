86631f1391d29ade061decad5a6fb4d1ed9c34cf
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-15 12:33:37 +0200

    P07: Done

---
+++ src/main/scala/P07.scala
@@ -0,0 +1,16 @@
+package org.bfn.ninetynineprobs
+
+object P07 {
+
+  private def flattenAcc[T](source : List[T], target : List[T]) : List[T] = source match {
+      case Nil => target
+      case (h:List[T]) :: tail =>
+        flattenAcc(tail, P05.reverse(flatten(h)) ::: target)
+      case h :: tail => flattenAcc(tail, h :: target)
+    }
+
+  /**
+   * Flatten a nested list structure
+   **/
+  def flatten[T](ls : List[T]) : List[T] = P05.reverse(flattenAcc(ls, Nil))
+}
