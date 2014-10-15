87585e50bed908a2c8626aa2a057a6099149f498
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-15 12:33:21 +0200

    P06: simplified tests

--- src/test/scala/P06Spec.scala
+++ src/test/scala/P06Spec.scala
@@ -8,33 +8,33 @@ class P06Spec extends UnitSpec {
     assert(P06.isPalindrome(List()))
   }
 
-  "isPalindrome" should "return true on an one-element list" in {
+  it should "return true on an one-element list" in {
     assert(P06.isPalindrome(List(2)))
   }
 
-  "isPalindrome" should "return true on an odd palindrome list" in {
+  it should "return true on an odd palindrome list" in {
     assert(P06.isPalindrome(List(2, 3, 2)))
     assert(P06.isPalindrome(List(1, 2, 3, 4, 3, 2, 1)))
     assert(P06.isPalindrome(List(1, 8, 3, 4, 3, 8, 1)))
   }
 
-  "isPalindrome" should "return true on an even palindrome list" in {
+  it should "return true on an even palindrome list" in {
     assert(P06.isPalindrome(List(2, 2)))
     assert(P06.isPalindrome(List(1, 2, 3, 4, 4, 3, 2, 1)))
     assert(P06.isPalindrome(List(1, 8, 3, 4, 4, 3, 8, 1)))
   }
 
-  "isPalindrome" should "return false on an odd non-palindrome list" in {
+  it should "return false on an odd non-palindrome list" in {
     assert(!P06.isPalindrome(List(1, 2, 3)))
     assert(!P06.isPalindrome(List(1, 2, 3, 4, 5)))
   }
 
-  "isPalindrome" should "return false on an even non-palindrome list" in {
+  it should "return false on an even non-palindrome list" in {
     assert(!P06.isPalindrome(List(1, 2)))
     assert(!P06.isPalindrome(List(1, 2, 3, 4, 6, 7)))
   }
 
-  "isPalindrome" should "work on String lists as well" in {
+  it should "work on String lists as well" in {
     assert(P06.isPalindrome(List("foo")))
     assert(P06.isPalindrome(List("foo", "qux", "foo")))
     assert(!P06.isPalindrome(List("foo", "bar")))
