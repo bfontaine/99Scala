03867d2cf17dee0cc5d7d6c14a8cf9a8673bf41d
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-15 09:55:21 +0200

    missing P01 javadoc
    [ci skip]

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -3,6 +3,10 @@ package org.bfn.ninetynineprobs
 import java.util.NoSuchElementException
 
 object P01 {
+
+  /**
+   * Find the last element of a list
+   **/
   def last[T](ls : List[T]) : T = ls match {
     case head :: Nil => head
     case head :: tail => last(tail)
