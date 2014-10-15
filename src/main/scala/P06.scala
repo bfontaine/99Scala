f5a737e05b1f23bfdae9e41964fc85e354552674
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-15 12:02:27 +0200

    P06: Done

---
+++ src/main/scala/P06.scala
@@ -0,0 +1,11 @@
+package org.bfn.ninetynineprobs
+
+object P06 {
+
+  /**
+   * Find out whether a list is a palindrome
+   **/
+  def isPalindrome(ls : List[_]) : Boolean = {
+    ls == P05.reverse(ls)
+  }
+}
