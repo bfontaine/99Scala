2207f42f5ab8da1112c2cf0469848deb229cd45a
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-16 13:53:46 +0200

    P19: Done

--- src/main/scala/P19.scala
+++ src/main/scala/P19.scala
@@ -2,6 +2,18 @@ package org.bfn.ninetynineprobs
 
 object P19 {
 
-  // TODO
+  /**
+   * Rotate a list N places to the left.
+   **/
+  def rotate[T](offset : Int, ls : List[T]) : List[T] = {
+    val len = P04.length(ls)
+
+    if (len == 0)
+      ls
+    else {
+      val (l, r) = P17.split(((offset % len) + len) % len, ls)
+      r ::: l
+    }
+  }
 
 }
