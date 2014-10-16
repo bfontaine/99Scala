f2a172fcc327938b29395c934c6e6a148dd17c72
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-16 09:54:34 +0200

    P16: Done

--- src/main/scala/P16.scala
+++ src/main/scala/P16.scala
@@ -2,6 +2,20 @@ package org.bfn.ninetynineprobs
 
 object P16 {
 
-  // TODO
+  def revDropAcc[T](n : Int, idx : Int, ls : List[T], acc : List[T])
+  : List[T] = ls match {
+    case Nil => acc
+    case h :: tail =>
+      if (idx%n == 0)
+        revDropAcc(n, idx+1, tail, acc)
+      else
+        revDropAcc(n, idx+1, tail, h :: acc)
+  }
+
+  /**
+   * Drop every Nth element from a list.
+   **/
+  def drop[T](n : Int, ls : List[T]) : List[T] =
+    P05.reverse(revDropAcc(n, 1, ls, Nil))
 
 }
