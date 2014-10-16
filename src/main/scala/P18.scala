b4537fb66fa678accfdfbe220376da240f782741
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-16 10:21:32 +0200

    P18: Done

--- src/main/scala/P18.scala
+++ src/main/scala/P18.scala
@@ -2,6 +2,16 @@ package org.bfn.ninetynineprobs
 
 object P18 {
 
-  // TODO
+  /**
+   * Extract a slice from a list.
+   * Given two indices, I and K, the slice is the list containing the elements
+   * from and including the Ith element up to but not including the Kth element
+   * of the original list. Start counting the elements with 0.
+   **/
+  def slice[T](start : Int, end : Int, ls : List[T]) : List[T] = {
+    val (_, tmp) = P17.split(start, ls)
+    val (middle, _) = P17.split(end - start, tmp)
+    middle
+  }
 
 }
