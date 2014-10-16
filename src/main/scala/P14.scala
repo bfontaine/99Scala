e351ecaa3c85ef8120ade574916360b04ffc2a0a
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-16 09:09:32 +0200

    P14: Done

--- src/main/scala/P14.scala
+++ src/main/scala/P14.scala
@@ -2,6 +2,16 @@ package org.bfn.ninetynineprobs
 
 object P14 {
 
-  // TODO
+  private def revDuplicateAcc[T](ls : List[T], acc : List[T]) : List[T] =
+    ls match {
+      case Nil => acc
+      case h :: tail => revDuplicateAcc(tail, h :: h :: acc)
+    }
+
+  /**
+   * Duplicate the elements of a list
+   **/
+  def duplicate[T](ls : List[T]) : List[T] =
+    P05.reverse(revDuplicateAcc(ls, Nil))
 
 }
