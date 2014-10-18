ee8da8e8fbaa1f1ad7a594b815fc3fafcca5eee4
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-18 22:38:56 +0200

    P26: tests added

--- src/main/scala/P26.scala
+++ src/main/scala/P26.scala
@@ -2,6 +2,15 @@ package org.bfn.ninetynineprobs
 
 object P26 {
 
-  // TODO
+  /**
+   * Generate the combinations of K distinct objects chosen from the N elements
+   * of a list.
+   * In how many ways can a committee of 3 be chosen from a group of 12 people?
+   * We all know that there are C(12,3) = 220 possibilities (C(N,K) denotes the
+   * well-known binomial coefficient). For pure mathematicians, this result may
+   * be great. But we want to really generate all the possibilities.
+   **/
+  def combinations[T](count : Int, ls : List[T]) : List[List[T]] =
+    Nil // TODO
 
 }
--- src/test/scala/P26Spec.scala
+++ src/test/scala/P26Spec.scala
@@ -4,7 +4,22 @@ import org.scalatest._
 
 class P26Spec extends UnitSpec {
 
-  // TODO
+  "combinations" should "return one empty list when N=0" in {
+    assert(P26.combinations(0, List(1, 2, 3)) == List(List()))
+  }
 
+  it should "return one list per item when N=1" in {
+    val myList = List(3, 2, 1, 5)
+    assert(P26.combinations(1, myList) == myList.map((x) => List(x)))
+  }
+
+  it should "return lists of length N" in {
+    assert(P26.combinations(4, List(1, 2, 3, 5, 4))(0).length == 4)
+  }
+
+  it should "return C(list.length, N) lists" in {
+    val myList = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+    assert(P26.combinations(3, myList).length == 220)
+  }
 }
 
