2534e6655d9128f91cb3d31becc6a0cb603dbdb1
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-22 12:47:49 +0200

    P34: Done

--- src/main/scala/P34.scala
+++ src/main/scala/P34.scala
@@ -2,6 +2,12 @@ package org.bfn.ninetynineprobs
 
 object P34 {
 
-  // TODO
+  private def totientAcc(m : Integer, r : Integer, acc : Integer) : Integer =
+    if (m <= 0 || r > m)
+      acc
+    else
+      totientAcc(m, r+1, if (P33.isCoprimeTo(r, m)) acc + 1 else acc)
+
+  def totient(m : Integer) = totientAcc(m, 1, 0)
 
 }
--- src/test/scala/P34Spec.scala
+++ src/test/scala/P34Spec.scala
@@ -4,7 +4,21 @@ import org.scalatest._
 
 class P34Spec extends UnitSpec {
 
-  // TODO
+  "totient" should "return 0 if m=0" in {
+    assert(P34.totient(0) == 0)
+  }
+
+  it should "return 0 if m<0" in {
+    assert(P34.totient(-12) == 0)
+  }
+
+  it should "return 1 if m=1" in {
+    assert(P34.totient(1) == 1)
+  }
+
+  it should "return m-1 if m is a prime number" in {
+    assert(P34.totient(17) == 16)
+  }
 
 }
 
