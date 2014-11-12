795007385f3927778a082342418ddf399e809e8b
Baptiste Fontaine <batifon@yahoo.fr>
2014-11-12 13:02:00 +0100

    P49: done

--- src/main/scala/P49.scala
+++ src/main/scala/P49.scala
@@ -2,6 +2,22 @@ package org.bfn.ninetynineprobs
 
 object P49 {
 
-  // TODO
+  /**
+   * An n-bit Gray code is a sequence of n-bit strings constructed according to
+   * certain rules. For example,
+   *  n = 1: C(1) = ("0", "1").
+   *  n = 2: C(2) = ("00", "01", "11", "10").
+   *  n = 3: C(3) = ("000", "001", "011", "010", "110", "111", "101", "100").
+   *
+   * Find out the construction rules and write a function to generate Gray
+   * codes.
+   **/
+  def gray(n : Integer) : List[String] = {
+    if (n == 0) List("")
+    else {
+      val g = gray(n-1)
+      g.map("0" + _) ::: g.reverse.map("1" + _)
+    }
+  }
 
 }
--- src/test/scala/P49Spec.scala
+++ src/test/scala/P49Spec.scala
@@ -4,7 +4,17 @@ import org.scalatest._
 
 class P49Spec extends UnitSpec {
 
-  // TODO
+  "gray" should "return (0, 1) for 1" in {
+    assert(P49.gray(1) == List("0", "1"))
+  }
+
+  it should "return (00, 01, 11, 10) for 2" in {
+    assert(P49.gray(2) == List("00", "01", "11", "10"))
+  }
+
+  it should "return (000, 001, 011, 010, 110, 111, 101, 100)" in {
+    assert(P49.gray(3) == List("000", "001", "011", "010", "110", "111", "101", "100"))
+  }
 
 }
 
